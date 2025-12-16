c806b884c50f5e0b608b7b5de351506976c9d772
Dmytro Paduchak <dmytro.paduchak@gmail.com>
2025-12-16 00:04:55 +0000

    fix

--- src/app/api/quote/route.ts
+++ src/app/api/quote/route.ts
@@ -1,24 +1,33 @@
-import { TemplateEmail } from '../../../templates/email-quote';
-import { Resend } from 'resend';
-import { ReactElement } from 'react';
+// import { TemplateEmail } from '../../../templates/email-quote';
+// import { Resend } from 'resend';
+// import { ReactElement } from 'react';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: Request) {
-  const json = await request.json();
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(nextRequest: NextRequest) {
+  const currentTime = Date.now();
   try {
-    const { data, error } = await resend.emails.send({
-      from: "MySMI.UK <quote@mysmi.uk>",
-      to: ["serhijkoschelnik@gmail.com"],
-      subject: "Quote Request",
-      react: TemplateEmail(json) as ReactElement,
-    });
+    const json = await nextRequest.json();
+    console.log(json)
+    // const { data, error } = await resend.emails.send({
+    //   from: "MySMI.UK <quote@mysmi.uk>",
+    //   to: ["serhijkoschelnik@gmail.com"],
+    //   subject: "Quote Request",
+    //   react: TemplateEmail(json) as ReactElement,
+    // });
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
-    return Response.json({ data });
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    // if (error) {
+    //   return Response.json({ error }, { status: 500 });
+    // }
+    // return Response.json({ data });
+    const time = Date.now() - currentTime;
+    console.log(`[DEBUG][API][POST][quote] ${time}ms.`);
+    return NextResponse.json({ status: 204 });
+  } catch (err) {
+    const time = Date.now() - currentTime;
+    console.log(`[ERROR][API][POST][quote] ${time}ms.`, err);
+    return NextResponse.json({ error: "Bad Request", status: 400 });
   }
 }
